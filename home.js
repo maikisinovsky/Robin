03a3c2d3602aa20ce52fd59651d9aa805b123ab5
maikisinovsky <maia.kisinovsky@gmail.com>
2024-08-13 15:05:13 -0300

    refactor: cambios en el html de la taskbar

--- home.js
+++ home.js
@@ -4,5 +4,6 @@ let button = document.getElementById('botonUsar');
 button.addEventListener("click", function() {
 
     document.getElementById("tablaRobin").innerHTML = "ejemplo";
+    ejemplo = 0
 
 });
