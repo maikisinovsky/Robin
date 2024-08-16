68557d38498a77736497101c47cfb49a998984ec
maikisinovsky <maia.kisinovsky@gmail.com>
2024-08-16 12:11:53 -0300

    test: implementacion del local storage en la home

--- home.js
+++ home.js
@@ -1,9 +1,34 @@
-let list = [];
-let button = document.getElementById('botonUsar');
 
-button.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    const listaDatosFormulario = JSON.parse(
+    localStorage.getItem("listaDatosFormulario")
+    );
 
-    document.getElementById("tablaRobin").innerHTML = "ejemplo";
-    ejemplo = 0
+    console.log(listaDatosFormulario)
+    const container = document.querySelector(".tablaRobin");
+    container.classList.add("datos");
 
-});
+    if (listaDatosFormulario && listaDatosFormulario.length > 0) {
+    container.innerHTML = `
+        <div class="grid-table">
+            <div class="grid-header">Nombre</div>
+            <div class="grid-header">Primer Destino</div>
+            <div class="grid-header">Segundo Destino</div>
+        </div>
+    `;
+
+        
+    listaDatosFormulario.forEach((datosFormulario) => {
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+        row.innerHTML = `
+            <div class="grid-cell">${datosFormulario.nombre}</div>
+            <div class="grid-cell">${datosFormulario.primerDestino}</div>
+            <div class="grid-cell">${datosFormulario.segundoDestino}</div>
+        `;
+        container.querySelector(".grid-table").appendChild(row);
+    });
+    } else {
+    container.innerHTML = `<h3>No hay actividad pendiente de robin</h3>`;    }
+
+    });
