a99a4bcf90d6014ec5e6d594756138a04aabb83d
maikisinovsky <maia.kisinovsky@gmail.com>
2024-08-19 10:36:36 -0300

    Hot fix: arreglo de errores en el log y sign

--- login.js
+++ login.js
@@ -1,13 +1,15 @@
-const loginform = document.querySelector("loginFrom")
-loginform.addEventListener(sumbit, (e)=> {
+const loginform = document.querySelector("#loginForm")
+loginform.addEventListener("submit", (e)=> {
     e.preventDefault()
     const Mail = document.querySelector("#Mail").value
     const Contraseña = document.querySelector("#Contraseña").value
     const Users = JSON.parse(localStorage.getItem("users")) || []
-    const validUser = User.find(user => user.Mail === Mail && user.Contraseña === Contraseña)
+    const validUser = Users.find(user => user.Mail === Mail && user.Contraseña === Contraseña)
     if(!validUser){ 
         return alert("Usuario y/o contraseña incorrectos")
-    }
+    } else {
     alert("Bienvenido!")
     window.location.href = "home.html"
+    }
+    
 }) 
