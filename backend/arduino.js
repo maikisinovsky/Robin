cdfa5860d95c19500da5740b85881fdd6ff9c2e3
maikisinovsky <maia.kisinovsky@gmail.com>
2024-11-29 14:27:31 -0300

    hot fix: arreglo de errores log

--- backend/arduino.js
+++ backend/arduino.js
@@ -1,6 +1,6 @@
 import { SerialPort } from 'serialport'
 
-const serialport = new SerialPort({ path: 'COM5', baudRate: 9600 })
+const serialport = new SerialPort({ path: 'COM6', baudRate: 9600 })
 
 export const arduino =(data)=>{
     serialport.write(data.toString())
